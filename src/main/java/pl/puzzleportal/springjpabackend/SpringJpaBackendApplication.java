59ddf04039731b56b0d6f0065b689e8946d322c0
Luke Hawthorne <mail@przykladowy.co.pl>
2020-02-08 14:03:14 +0100

    Initial commit

---
+++ src/main/java/pl/puzzleportal/springjpabackend/SpringJpaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.puzzleportal.springjpabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/puzzleportal/springjpabackend/SpringJpaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.puzzleportal.springjpabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
