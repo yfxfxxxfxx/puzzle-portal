b047d562f17a143821ceb2aff34d81e8d3977f3f
yfxfxxxfxx <luke.hawthorne@interia.pl>
2020-02-09 12:41:58 +0100

    Kontroler dla uzytkownikow zaimplementowany

--- src/main/java/pl/puzzleportal/springjpabackend/datasource/DataSourceConfig.java
+++ src/main/java/pl/puzzleportal/springjpabackend/datasource/DataSourceConfig.java
@@ -12,6 +12,8 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:test");
         dataSourceBuilder.username("SA");
         dataSourceBuilder.password("");
         return dataSourceBuilder.build();
